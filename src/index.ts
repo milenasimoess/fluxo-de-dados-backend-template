06df69658a45d1157e60f5c193d5ed49851817a8
milenarismo <milenasimoeslabenu@gmail.com>
2023-01-11 13:46:16 -0300

    Finalizado

--- src/index.ts
+++ src/index.ts
@@ -21,32 +21,127 @@ app.get("/accounts", (req: Request, res: Response) => {
 })
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
+
+   try{
     const id = req.params.id
-
     const result = accounts.find((account) => account.id === id) 
-
+ 
+    if(!result){
+    res.status(404)
+    throw new Error("Conta não existe, verifique o id")
+    }
     res.status(200).send(result)
+   } catch(error:any) {
+    console.log(error)
+
+    if(res.statusCode === 200){
+        res.status(500)
+    }
+    res.send(error.message)
+
+   }
+
+
+   
+    
 })
+
+// Refatorar para o uso do bloco try/catch
+
+// Validação de input:
+// Caso a id recebida não inicie com a letra ‘a’ será retornado um erro 400
+// Mensagem de erro:
+// 	“‘id’ inválido. Deve iniciar com letra ‘a’”
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
+          
+        if(id[0]!== "a"){
+            res.status(400)
+            throw new Error(" id inválido. Deve iniciar com a letra a.");
+        }
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+        const accountIndex = accounts.findIndex((account) => account.id === id)
+    
+        if (accountIndex >= 0) {
+            accounts.splice(accountIndex, 1)
+            res.status(200).send("Item deletado com sucesso")
+        } else {
+            res.status(404).send("Item não encontrado")
+        }
+    
+        
+        
+    } catch (error) {
+        
+        console.log(error)
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+        res.send(error.message)
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
     }
-
-    res.status(200).send("Item deletado com sucesso")
+   
 })
 
-app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+// req.body.balance (newBalance)
+// Deve ser number
+// Deve ser maior ou igual a zero
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+// req.body.type (newType)
+// deve valer um dos tipos de conta do enum
+
+app.put("/accounts/:id", (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+
+//         Para fixar, agora é sua vez de implementar as seguintes validações:
+// req.body.id (newId)
+// string que inicia com a letra ‘a’
+
+// req.body.ownerName (newOwnerName)
+// string com no mínimo 2 caracteres
+
+    const newId = req.body.id 
+    if(newId !== undefined) {
+        if(newId[0] !== "a"){
+            res.status(400)
+            throw new Error ("A primeira letra deve ser a")
+        }
+    }
+    const newOwnerName = req.body.ownerName 
+    if(newOwnerName !== undefined){
+        if (newOwnerName.length < 2){
+        res.status(400)
+            throw new Error ("Deve ter pelo menos dois caracteres")}
+    }
+    const newBalance = req.body.balance 
+    if(typeof newBalance !== "number"){
+        res.status(400)
+        throw new Error("Balance deve ser do tipo number");
+    }
+    if(newBalance<0){
+      res.status(400)
+      throw new Error("'balance' não pode ser negativo")
+    }
+
+    //req.body.type (newType)
+    // deve valer um dos tipos de conta do enum
+
+    const newType = req.body.type 
+    if(newType !== undefined){
+
+        if(newType !== ACCOUNT_TYPE.GOLD &&
+            newType !== ACCOUNT_TYPE.PLATINUM &&
+            newType !== ACCOUNT_TYPE.BLACK){
+                res.status(400)
+                throw new Error("'type' deve ser um tipo válido: gold, platinium ou black")
+
+        }
+    }
+
+
 
     const account = accounts.find((account) => account.id === id) 
 
@@ -59,4 +154,14 @@ app.put("/accounts/:id", (req: Request, res: Response) => {
     }
 
     res.status(200).send("Atualização realizada com sucesso")
+        
+    } catch (error) {
+        console.log(error)
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+        res.send(error.message)
+        
+    }
+    
 })
